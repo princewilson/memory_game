859e4c26b49160e978963456e53294a22dfbc5ce
Prince Wilson <yourcoolprince@gmail.com>
2022-11-04 06:02:56 +0530

    arranged cards in 4 x 4 and randomized the order

--- ToDo.md
+++ ToDo.md
@@ -1,4 +1,4 @@
 # ToDo
 
 - <strike>Learn how to flip images</strike>
-- Arrange all the images in an n x n matrix, and make sure the order is random
+- <strike>Arrange all the images in a 4 x 4 matrix, and make sure the order is random</strike>
--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const back_image = document.querySelector(".back");
-back_image.addEventListener("click", () => {
-    back_image.classList.add("rotate");
-    document.querySelector(".card").classList.add("rotate");
+const back_images = document.querySelectorAll(".back");
+back_images.forEach((back_image) => {
+    back_image.addEventListener("click", () => {
+        back_image.classList.add("rotate");
+        back_image.parentElement.classList.add("rotate");
+    })
+});
+
+window.addEventListener("load", () => {
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+        card.style.order = Math.floor(Math.random() * 16);;
+    });
 });
