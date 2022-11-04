5d48221cf3cc80653f5ec4660c540705a6c63aeb
Prince Wilson <yourcoolprince@gmail.com>
2022-11-04 19:23:20 +0530

    two cards flipped, if not same then flipped back

--- ToDo.md
+++ ToDo.md
@@ -2,3 +2,4 @@
 
 - <strike>Learn how to flip images</strike>
 - <strike>Arrange all the images in a 4 x 4 matrix, and make sure the order is random</strike>
+- When two cards are flipped, if they are not the same, then flip them back otherwise let them be.
--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+var counter = 0;
+var card1;
+var card2;
+
 const back_images = document.querySelectorAll(".back");
 back_images.forEach((back_image) => {
     back_image.addEventListener("click", () => {
+        counter += 1;
         back_image.classList.add("rotate");
         back_image.parentElement.classList.add("rotate");
+        if (counter === 1) {
+            card1 = back_image.parentElement.firstElementChild;
+        }
+        if (counter === 2) {
+            card2 = back_image.parentElement.firstElementChild;
+            if (card1.alt === card2.alt) {
+
+            } else {
+                setTimeout(() => {
+                    card1.parentElement.classList.remove("rotate");
+                    card1.parentElement.lastElementChild.classList.remove("rotate");
+                    card2.parentElement.classList.remove("rotate");
+                    card2.parentElement.lastElementChild.classList.remove("rotate");
+                }, 1000);
+            }
+            counter = 0;
+        }
     })
 });
 
